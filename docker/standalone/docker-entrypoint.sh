9e571392a736ca152b4c34d4868287942ca37d6d
Nelson Chu <csnelsonchu@gmail.com>
2022-11-15 10:10:44 +0800

    move installation to redis after proxy starts

--- docker/standalone/docker-entrypoint.sh
+++ docker/standalone/docker-entrypoint.sh
@@ -348,9 +348,9 @@ _main() {
 	# ignore failure since there are cases where we can't chmod (and PostgreSQL might fail later anyhow - it's picky about permissions of this directory)
 	chmod 700 "$MLDATA" || :
 	chmod 700 "$LOGDATA" || :
-	nohup redis-server >> "$LOGDATA"/redis-server.out 2>&1 &
 	nohup python3 /usr/local/bin/superinsight/proxy/main.py >> "$LOGDATA"/proxy.out 2>&1 &
 	nohup python3 /usr/local/bin/superinsight/server/main.py  >> "$LOGDATA"/server-index.out 2>&1 &
+	nohup redis-server >> "$LOGDATA"/redis-server.out 2>&1 &
 	nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --chdir /usr/local/bin/superinsight/server -w 1 --bind localhost:8081>> "$LOGDATA"/server.out 2>&1 &
 	nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --chdir /usr/local/bin/superinsight/search -w 1 --bind localhost:8082>> "$LOGDATA"/search.out 2>&1 &
 	nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --chdir /usr/local/bin/superinsight/predict/transformers -w 1 --bind localhost:8084>> "$LOGDATA"/predict-transformers.out 2>&1 &
