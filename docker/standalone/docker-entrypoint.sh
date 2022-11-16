9f248f03ecd8ce04ac1100608d677dd0d51c1966
Nelson Chu <csnelsonchu@gmail.com>
2022-11-16 17:35:30 +0800

    copy redis.conf to default path

--- docker/standalone/docker-entrypoint.sh
+++ docker/standalone/docker-entrypoint.sh
@@ -346,9 +346,11 @@ _main() {
 	# ignore failure since there are cases where we can't chmod (and PostgreSQL might fail later anyhow - it's picky about permissions of this directory)
 	chmod -R 777 "$MLDATA" || :
 	chmod -R 777 "$LOGDATA" || :
+	mkdir -p /etc/redis
 	nohup python3 /usr/local/bin/superinsight/proxy/main.py >> "$LOGDATA"/proxy.out 2>&1 &
 	nohup python3 /usr/local/bin/superinsight/server/main.py  >> "$LOGDATA"/server-index.out 2>&1 &
-	nohup redis-server "$REDISDATA" >> "$LOGDATA"/redis-server.out 2>&1 &
+	cp "$REDISDATA"/redis.conf /etc/redis/redis.conf
+	nohup redis-server >> "$LOGDATA"/redis-server.out 2>&1 &
 	nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --chdir /usr/local/bin/superinsight/server -w 1 --bind localhost:8081>> "$LOGDATA"/server.out 2>&1 &
 	nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --chdir /usr/local/bin/superinsight/search -w 1 --bind localhost:8082>> "$LOGDATA"/search.out 2>&1 &
 	nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --chdir /usr/local/bin/superinsight/predict/transformers -w 1 --bind localhost:8084>> "$LOGDATA"/predict-transformers.out 2>&1 &
