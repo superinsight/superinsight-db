69f541b7d547b3b0ff7d9bc309837014d3eb9c88
Nelson Chu <csnelsonchu@gmail.com>
2022-12-16 04:25:43 +0800

    remove print line

--- src/search/main.py
+++ src/search/main.py
@@ -8,28 +8,6 @@ import time
 storage = Environment.default_storage
 default_storage = StorageLocation.LOCAL_DISK
 version = "0.9.2"
-config.dictConfig({
-    "version": 1,
-    "root": {
-        "handlers": ["console"],
-        "level": "DEBUG"
-    },
-    "handlers": {
-        "console": {
-            "formatter": "std_out",
-            "class": "logging.StreamHandler",
-            "level": "DEBUG"
-        }
-    },
-    "formatters": {
-        "std_out": {
-            "format": "%(asctime)s : %(levelname)s : %(module)s : %(funcName)s : %(lineno)d : (Process Details : (%(process)d, %(processName)s), Thread Details : (%(thread)d, %(threadName)s))\nLog : %(message)s",
-            "datefmt": "%d-%m-%Y %I:%M:%S"
-        }
-    },
-})
-
-
 def main():
     if Environment.kafka_topic_to_consume.upper() == ConsumeTopic.DIVIDE.value:
         DatabaseConsumer().consume(topics=Environment.kafka_topic_divide,
@@ -42,7 +20,6 @@ def main():
                                  storage_location=StorageLocation.LOCAL_DISK)
     if Environment.kafka_topic_to_consume.upper() == ConsumeTopic.ALL.value:
         topics = [Environment.kafka_topic_divide, Environment.kafka_topic_conquer, Environment.kafka_topic_combine]
-        print(topics)
         DatabaseConsumer().consume(topics=topics,
                                    storage_location=StorageLocation.LOCAL_DISK)
     if Environment.kafka_topic_to_consume.upper() == ConsumeTopic.NONE.value:
