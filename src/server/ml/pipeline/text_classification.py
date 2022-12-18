a24351e07556dc073f848e6e5b12c2123b9e4634
Nelson Chu <csnelsonchu@gmail.com>
2022-12-18 09:58:38 +0800

    format and add common logger

--- src/server/ml/pipeline/text_classification.py
+++ src/server/ml/pipeline/text_classification.py
@@ -1,40 +1,53 @@
-import logging
 import requests
 import json
 from environment import Environment
+from common.logger import CommonLogger
+
+
 class TextClassificationPipeline:
 
-    logger = None
+    logger = CommonLogger()
     provider = None
 
-    def __init__(self, logger=None, handler = None, provider = None):
-      self.logger = logger or logging.getLogger(__name__)
-      self.logger.setLevel(logging.INFO)
-      if handler is not None:
-        self.logger.addHandler(handler)
-      self.provider = provider
-      self.logger.info("TextClassificationPipeline init for provider:{}".format(str(self.provider)))
+    def __init__(self, logger=None, handler=None, provider=None):
+        self.provider = provider
+        self.logger.info(
+            "TextClassificationPipeline init for provider:{}".format(str(self.provider))
+        )
 
     def __del__(self):
-      self.logger.info("TextClassificationPipeline exiting for provider:{}".format(str(self.provider)))
+        self.logger.info(
+            "TextClassificationPipeline exiting for provider:{}".format(
+                str(self.provider)
+            )
+        )
 
     def run(self, inputs, labels):
-      return self.__run_superinsight(inputs = inputs, labels = labels)
+        return self.__run_superinsight(inputs=inputs, labels=labels)
 
-    def __run_superinsight(self, inputs = None, labels = []):
-      model_inputs = {
-          "inputs":  inputs,
-          "model": Environment.default_model_text_classification, 
-          "parameters": {"candidate_labels": labels },
-          "options": {
-            "use_gpu": False,
-            "wait_for_model": True
-          }
-      }
-      self.logger.info("Init ML.Pipeline.TextClassification.__run_superinsight:model_inputs", model_inputs)
-      url = "{}/zero-shot-classification".format(Environment.provider_api_url_superinsight)
-      response = requests.request("POST", url, data=json.dumps(model_inputs), headers = {"accept": "application/json", "Content-Type": "application/json" })
-      model_output = response.json()
-      self.logger.info("Init ML.Pipeline.TextClassification.__run_superinsight:model_output")
-      self.logger.info(json.dumps({"model_output":model_output}))
-      return model_output
+    def __run_superinsight(self, inputs=None, labels=[]):
+        model_inputs = {
+            "inputs": inputs,
+            "model": Environment.default_model_text_classification,
+            "parameters": {"candidate_labels": labels},
+            "options": {"use_gpu": False, "wait_for_model": True},
+        }
+        self.logger.info(
+            "Init ML.Pipeline.TextClassification.__run_superinsight:model_inputs",
+            model_inputs,
+        )
+        url = "{}/zero-shot-classification".format(
+            Environment.provider_api_url_superinsight
+        )
+        response = requests.request(
+            "POST",
+            url,
+            data=json.dumps(model_inputs),
+            headers={"accept": "application/json", "Content-Type": "application/json"},
+        )
+        model_output = response.json()
+        self.logger.info(
+            "Init ML.Pipeline.TextClassification.__run_superinsight:model_output"
+        )
+        self.logger.info(json.dumps({"model_output": model_output}))
+        return model_output
--- src/server/ml/pipeline/text_generation.py
+++ src/server/ml/pipeline/text_generation.py
@@ -1,41 +1,50 @@
-import logging
 import requests
 import json
 from environment import Environment
+from common.logger import CommonLogger
+
+
 class TextGenerationPipeline:
 
-    logger = None
+    logger = CommonLogger()
     provider = None
 
-    def __init__(self, logger=None, handler = None, provider = None):
-      self.logger = logger or logging.getLogger(__name__)
-      self.logger.setLevel(logging.INFO)
-      if handler is not None:
-        self.logger.addHandler(handler)
-      self.provider = provider
-      self.logger.info("Init ML.Pipeline.TextGeneration for provider:{}".format(str(provider)))
+    def __init__(self, logger=None, handler=None, provider=None):
+        self.provider = provider
+        self.logger.info(
+            "Init ML.Pipeline.TextGeneration for provider:{}".format(str(provider))
+        )
 
     def __del__(self):
-      self.logger.info("Exit ML.Pipeline.TextGeneration...")
+        self.logger.info("Exit ML.Pipeline.TextGeneration...")
 
     def run(self, prompt, max_length, min_length):
-      return self.__run_superinsight(prompt = prompt, max_length=max_length, min_length=min_length)
+        return self.__run_superinsight(
+            prompt=prompt, max_length=max_length, min_length=min_length
+        )
 
     def __run_superinsight(self, prompt, max_length, min_length):
-      model_inputs = {
-          "inputs":  prompt,
-          "model": Environment.default_model_text_generation, 
-          "max_length": max_length,
-          "min_length": min_length,
-          "options": {
-            "use_gpu": False,
-            "wait_for_model": True
-          }
-      }
-      self.logger.info("Init ML.Pipeline.TextGeneration.__run_superinsight:model_inputs", model_inputs)
-      url = "{}/text-generation".format(Environment.provider_api_url_superinsight)
-      response = requests.request("POST", url, data=json.dumps(model_inputs), headers = {"accept": "application/json", "Content-Type": "application/json" })
-      model_output = response.json()
-      self.logger.info("Init ML.Pipeline.TextGeneration.__run_superinsight:model_output")
-      self.logger.info(json.dumps({"model_output":model_output}))
-      return model_output["output"]
+        model_inputs = {
+            "inputs": prompt,
+            "model": Environment.default_model_text_generation,
+            "max_length": max_length,
+            "min_length": min_length,
+            "options": {"use_gpu": False, "wait_for_model": True},
+        }
+        self.logger.info(
+            "Init ML.Pipeline.TextGeneration.__run_superinsight:model_inputs",
+            model_inputs,
+        )
+        url = "{}/text-generation".format(Environment.provider_api_url_superinsight)
+        response = requests.request(
+            "POST",
+            url,
+            data=json.dumps(model_inputs),
+            headers={"accept": "application/json", "Content-Type": "application/json"},
+        )
+        model_output = response.json()
+        self.logger.info(
+            "Init ML.Pipeline.TextGeneration.__run_superinsight:model_output"
+        )
+        self.logger.info(json.dumps({"model_output": model_output}))
+        return model_output["output"]
