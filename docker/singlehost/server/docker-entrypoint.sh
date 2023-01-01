a7045fba2e8227beff144f9e37edf9497d23f616
Nelson Chu <csnelsonchu@gmail.com>
2023-01-01 15:38:49 +0800

    fix path issue

--- docker/singlehost/server/docker-entrypoint.sh
+++ docker/singlehost/server/docker-entrypoint.sh
@@ -37,8 +37,10 @@ docker_create_db_directories() {
 	local user; user="$(id -u)"
 
 	mkdir -p "$PGDATA"
+	mkdir -p "$MLDATA"
 	# ignore failure since there are cases where we can't chmod (and PostgreSQL might fail later anyhow - it's picky about permissions of this directory)
 	chmod 700 "$PGDATA" || :
+	chmod 777 "$MLDATA" || :
 
 	# ignore failure since it will be fine when using the image provided directory; see also https://github.com/docker-library/postgres/pull/289
 	mkdir -p /var/run/postgresql || :
@@ -343,12 +345,12 @@ _main() {
 	fi
 	################################################################################################################################################
 	echo 'Starting Superinsight Machine Learning Layers.....'
-	# ignore failure since there are cases where we can't chmod (and PostgreSQL might fail later anyhow - it's picky about permissions of this directory)
-	chmod -R 777 "$MLDATA" || :
-	chmod -R 777 "$LOGDATA" || :
 	nohup python3 /usr/local/bin/superinsight/proxy/main.py &
 	nohup python3 /usr/local/bin/superinsight/server/main.py &
+	#nohup python3 /usr/local/bin/superinsight/search/main.py &
 	nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --chdir /usr/local/bin/superinsight/server -t 0 -w 1 --bind localhost:8081 &
+	#nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --chdir /usr/local/bin/superinsight/search -t 0 -w 1 --bind localhost:8082 &
+	#nohup gunicorn -k uvicorn.workers.UvicornWorker app:app --chdir /usr/local/bin/superinsight/predict/transformers -t 0 -w 1 --bind localhost:8084 &
 	echo 'Superinsight is now starting up.....'
 	echo '################################################################################################################################################'
 	exec "$@"
